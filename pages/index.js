f03bbf3dc7b82de5a061460c82b8c6a4c1c6f9f5
marcmec <viniciuslag@gmail.com>
2022-06-03 02:14:03 -0300

    configuration project

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+const Page = () => {
+    return (
+        <Container>
+            <Box
+                borderRadius="lg"
+                bg="#006400"
+                p={3}
+                align="center"
+                color="white"
+            >
+                Salve, sou desenvolvedor full-stack, moro no Brasil!
+            </Box>
+            <Box display={{ md: "flex" }}>
+                <Box flexGrow={1} as="h2" variant="page-title">
+                    <Heading>Marcos Vinícius</Heading>
+                    <p>Talker | Engenheiro / Desenvolvedor </p>
+                </Box>
+            </Box>
+        </Container>
+    )
+}
+
+export default Page
