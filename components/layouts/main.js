2c2ff698b14d3914824c23093c81471cc768b4da
marcmec <viniciuslag@gmail.com>
2022-06-04 22:01:13 -0300

    feature: add myroom animation into project with three.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,12 @@
 import Head from "next/head"
 import { Box, Container } from "@chakra-ui/react"
 import Navbar from "../navbar"
+import dynamic from "next/dynamic"
+import VoxeRoomLoader from "../vox-room-loader"
+const LazyLoopRoom = dynamic(() => import("../load-room"), {
+    ssr: false,
+    loading: () => <VoxeRoomLoader />
+})
 const Main = ({ children, router }) => {
     return (
         <Box as="main" pb={8}>
@@ -14,6 +20,7 @@ const Main = ({ children, router }) => {
             <Navbar path={router.asPath} />
 
             <Container maxW="container.md" pt={14}>
+                <LazyLoopRoom />
                 {children}
             </Container>
         </Box>
